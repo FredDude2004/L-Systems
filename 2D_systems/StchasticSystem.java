f1d0ceeee3c075a901d77d774c25781acf522baf
FredDude <kalebduchesneau@gmail.com>
2025-09-22 16:05:26 -0500

    import statements

---
+++ 2D_systems/StchasticSystem.java
@@ -0,0 +1,17 @@
+import renderer.models_L.turtlegraphics.*;
+
+/*
+ * L-
+*/
+
+public class StochasticSystems {
+    String axiom = "";
+
+    void expand() {
+        // expand the string using the rules
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
