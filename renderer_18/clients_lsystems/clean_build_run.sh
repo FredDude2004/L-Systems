9efa5a5061d916aea7778acfadb3f0c27cb7fb77
FredDude <kalebduchesneau@gmail.com>
2025-10-18 18:56:17 -0500

    created a test for the Turtle3D class

--- renderer_18/clients_lsystems/clean_build_run.sh
+++ renderer_18/clients_lsystems/clean_build_run.sh
@@ -1,6 +1,7 @@
 rm *.ppm
 rm *.class
 
-javac -g -Xlint -Xdiags:verbose -cp .:.. DOLSystem.java
-java -cp .:.. DOLSystem
+
+javac -g -Xlint -Xdiags:verbose -cp .:.. Turtle3DTest.java
+java -cp .:.. Turtle3DTest
 
