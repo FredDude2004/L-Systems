25e8ee3a4b4f2666267b3a5b1645b540c24a0c47
Kaleb Duchesneau <kalebduchesneau@gmail.com>
2025-11-24 08:51:38 -0600

    yeah yeah yeah

--- src/clients_lsystems/StochasticSystems.java
+++ src/clients_lsystems/StochasticSystems.java
@@ -15,10 +15,10 @@ public class StochasticSystems {
 	public static void main(String[] args) {
 		final Scene scene = new Scene("Stochastic L-Systems");
 		scene.addPosition(new Position(new Model(), "p0"));
-		scene.camera.viewTranslate(5, 0, 15);
+		scene.camera.viewTranslate(0, 5, 5);
 
-		final int width = 1024;
-		final int height = 1024;
+		final int width = 256;
+		final int height = 256;
 		final FrameBuffer fb = new FrameBuffer(width, height, Color.white);
 
 		for (int i = 0; i < 100; i++) {
@@ -27,7 +27,7 @@ public class StochasticSystems {
 		    scene.getPosition(0).setModel(binTree);
 		    fb.clearFB();
 		    Pipeline.render(scene, fb);
-		    fb.dumpFB2File(String.format("A%03d", i + 1));
+		    fb.dumpFB2File(String.format("A%03d.png", i + 1), "png");
 		}
 		
 		for (int i = 0; i < 100; i++) {
@@ -36,17 +36,19 @@ public class StochasticSystems {
 		    scene.getPosition(0).setModel(fern);
 		    fb.clearFB();
 		    Pipeline.render(scene, fb);
-		    fb.dumpFB2File(String.format("B%03d", i + 1));
+		    fb.dumpFB2File(String.format("B%03d.png", i + 1), "png");
 		}
 		
 		for (int i = 0; i < 100; i++) {
-		    Model flower = StochasticLSystem.flower(3);
+		    Model flower = StochasticLSystem.flower(6);
 		    ModelShading.setColor(flower, Color.black);
 		    scene.getPosition(0).setModel(flower);
 		    fb.clearFB();
 		    Pipeline.render(scene, fb);
-		    fb.dumpFB2File(String.format("D%03d", i + 1));
+		    fb.dumpFB2File(String.format("C%03d.png", i + 1), "png");
 		}
+
+		scene.camera.viewTranslate(0, 5, 10);
 		
 		for (int i = 0; i < 100; i++) {
 		    Model coral = StochasticLSystem.coral(5);
@@ -54,7 +56,7 @@ public class StochasticSystems {
 		    scene.getPosition(0).setModel(coral);
 		    fb.clearFB();
 		    Pipeline.render(scene, fb);
-		    fb.dumpFB2File(String.format("E%03d", i + 1));
+		    fb.dumpFB2File(String.format("D%03d.png", i + 1), "png");
 		}
 		
 	}
